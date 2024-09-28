e5373836c9bfec51b1a8626614b0fd49389444cc
EmulationNerds685 <bhaskartiwari270@gmail.com>
2024-09-28 20:40:16 +0530

    Initial

---
+++ index.js
@@ -0,0 +1,40 @@
+import express from 'express'
+import ejs from 'ejs'
+import axios from 'axios'
+import bodyParser from 'body-parser'
+import pg from 'pg'
+const port = 3500
+const app = express()
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static('public'))
+
+const apikey = "AIzaSyAWQt3Zdk3WpN7a4cgKuNwQ97qpCzz3Yk8"
+const db = new pg.Client({
+	user: 'postgres',
+	host: 'localhost',
+	database: 'Task',
+	password: 'Krusty685',
+	port: 5432
+})
+db.connect()
+app.get('/', async (req, res) => {
+	res.render('index.ejs')
+})
+
+app.post('/fetch', async (req, res) => {
+	try {
+		const response = await axios.get('https://www.googleapis.com/books/v1/volumes?q=' + req.body.book + '&key=' + apikey)
+		const result = response.data
+
+		res.render('index.ejs', { data: result.items })
+	}
+	catch (error) {
+		res.render('index.ejs',{error:error.message})
+	}
+
+})
+
+app.listen(port, (req, res) => {
+	console.log(`Server is running on port: http://localhost:${port}`)
+})
+
